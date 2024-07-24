483e10161f7e49275930f1efc507e7cfc0f36c3e
Thiago <thiagobsn@gmail.com>
2024-07-23 22:15:36 -0300

    Ajuste token swagger

--- src/main/java/com/fiap/agenda/application/medico/controller/AgendaController.java
+++ src/main/java/com/fiap/agenda/application/medico/controller/AgendaController.java
@@ -26,10 +26,12 @@ import com.fiap.agenda.domain.usecase.IConfirmarAgendamento;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@SecurityRequirement(name = "bearerAuth")
 @Tag(name = "Agenda")
 @Validated
 @RequiredArgsConstructor
